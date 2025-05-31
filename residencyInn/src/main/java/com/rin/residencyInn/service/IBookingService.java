02b17b22c4835a08f5e46f4d30b1b333662b8373
riya <riyagupta84715@gmail.com>
2025-05-31 23:01:55 +0530

    commit with frontend and backend

---
+++ residencyInn/src/main/java/com/rin/residencyInn/service/IBookingService.java
@@ -0,0 +1,15 @@
+package com.rin.residencyInn.service;
+
+import com.rin.residencyInn.model.BookedRoom;
+
+import java.util.List;
+
+public interface IBookingService {
+    void cancelBooking(Long bookingId);
+
+    String saveBooking(Long roomId, BookedRoom bookingRequest);
+
+    BookedRoom findByBookingConfirmationCode(String confirmationCode);
+
+    List<BookedRoom> getAllBookings();
+}
---
+++ residencyInn/src/main/java/com/rin/residencyInn/service/IUserService.java
@@ -0,0 +1,12 @@
+package com.rin.residencyInn.service;
+
+import com.rin.residencyInn.model.User;
+
+import java.util.List;
+
+public interface IUserService {
+    User registerUser(User user);
+    List<User> getUsers();
+    void deleteUser(String email);
+    User getUser(String email);
+}
